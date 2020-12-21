e88b40b9aa31a7cde96e6fce32c8c13fda1d5a1f
Calme1709 <callumlaw1709@outlook.com>
2020-12-21 17:01:50 +1300

    Use index signature instead of record to fix circular reference typescript error

--- src/routes/handler.ts
+++ src/routes/handler.ts
@@ -1,9 +1,12 @@
 import { RequestHandler } from "express";
 
-type RequestQuery = Record<string, string | string[] | RequestQuery | RequestQuery[]>;
+//eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+interface IRequestQuery {
+	[key: string]: string | string[] | IRequestQuery | IRequestQuery[];
+}
 
 export type Handler<
 	ReqBody extends Record<string, any> = Record<string, string>,
-	ReqParams extends RequestQuery = RequestQuery,
+	ReqParams extends IRequestQuery = IRequestQuery,
 	ResBody extends Record<string, any> = Record<string, string>
 > = RequestHandler<Record<string, string>, ResBody, ReqBody, ReqParams>;
