ef8dc4b6f845d527dbd81431ceb791853f32fa54
Calme1709 <callumlaw1709@outlook.com>
2021-01-13 02:39:35 +1300

    Use docker-compose for running the application

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,10 +1,10 @@
 export const database = {
-	url: "mongodb://localhost:27017",
+	url: "mongodb://mongo:27017",
 	name: "SSLManager"
 };
 
 export const TheSSLStoreURL = "https://sandbox-wbapi.thesslstore.com/";
 
 export const jwtSecret = "hello";
-export const port = 8080;
+export const port = 80;
 export const logFile = "/var/log/SSLManager.log";
