a7b6ac668a0eb6ed835c8ddb30fe21a0199526c6
Calme1709 <callumlaw1709@outlook.com>
2021-01-13 02:11:37 +1300

    Define and call a function instead of using an IIFE

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,9 +2,14 @@ import app from "./app";
 import { port, database } from "@config";
 import mongoose from "mongoose";
 
-//eslint-disable-next-line @typescript-eslint/no-floating-promises
-(async () => {
+/**
+ * The main function of the program, used due to lack of top level await when compiling for CommonJS Modules.
+ */
+const main = async () => {
 	await mongoose.connect(database.url, { useNewUrlParser: true, useUnifiedTopology: true, dbName: database.name, useCreateIndex: true });
 
 	app.listen(process.env.PORT ?? port);
-})();
+};
+
+//eslint-disable-next-line @typescript-eslint/no-floating-promises
+main();
