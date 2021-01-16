1069f8cdb1586c1e60ff2655f60f7329f2206ae7
Calme1709 <callumlaw1709@outlook.com>
2021-01-17 01:32:25 +1300

    Back: Ensure that OpenSSL is installed on startup

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,19 @@
 import app from "./app";
 import { port, database } from "@config";
 import mongoose from "mongoose";
+import { execSync } from "child_process";
 
 /**
  * The main function of the program, used due to lack of top level await when compiling for CommonJS Modules.
  */
 const main = async () => {
+	//eslint-disable-next-line @typescript-eslint/naming-convention
+	const openSSLVersion = execSync("openssl version").toString("utf-8");
+
+	if(!/OpenSSL \d+\.\d+\.\d+[a-z]+.+$/g.exec(openSSLVersion)) {
+		throw new Error("OpenSSL not installed");
+	}
+
 	await mongoose.connect(database.url, { useNewUrlParser: true, useUnifiedTopology: true, dbName: database.name, useCreateIndex: true });
 
 	app.listen(process.env.PORT ?? port);
