f2e2fe8c657979e6ee0f11cc8f88156705bf8dd1
Calme1709 <callumlaw1709@outlook.com>
2021-01-17 01:32:44 +1300

    Back: Add staging values to the config

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,10 +1,10 @@
 export const database = {
-	url: "mongodb://mongo:27017",
+	url: process.env.NODE_ENV === "staging" ? "mongodb://localhost:27017" : "mongodb://mongo:27017",
 	name: "SSLManager"
 };
 
 export const TheSSLStoreURL = "https://sandbox-wbapi.thesslstore.com/";
 
 export const jwtSecret = "hello";
-export const port = 80;
+export const port = process.env.NODE_ENV === "staging" ? 8080 : 80;
 export const logFile = "/var/log/sslmanager/backend.log";
